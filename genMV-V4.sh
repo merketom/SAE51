1d097a80faaef15fcc76f48976c0293b84065b97
merketom <tom.merkel@univ-rouen.fr>
2025-09-09 14:23:24 +0200

    V4 du SH

--- genMV-V4.sh
+++ genMV-V4.sh
@@ -1,14 +1,16 @@
 arg1=$1
 arg2=$2
+ram=4096
+dd=65536
 
 if [ "$arg1" = "L" ]; then
     vboxmanage list vms
 
 elif [ "$arg1" = "N" ]; then
     vboxmanage createvm --name="$arg2" --ostype="Debian_64" --register
-    vboxmanage modifyvm "$arg2" --memory 4096 --cpus 2 --vram 64
+    vboxmanage modifyvm "$arg2" --memory "$ram" --cpus 2 --vram 64
 
-    vboxmanage createmedium disk --filename "$HOME/VirtualBox VMs/$arg2/$arg2.vdi" --size 65536 --format VDI
+    vboxmanage createmedium disk --filename "$HOME/VirtualBox VMs/$arg2/$arg2.vdi" --size "$dd" --format VDI
     vboxmanage storagectl "$arg2" --name "SATA Controller" --add sata --controller IntelAhci
     vboxmanage storageattach "$arg2" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/$arg2/$arg2.vdi"
 
