ae85bd449014014d201bfee34fccfb4e16ed7ba2
merketom <tom.merkel@univ-rouen.fr>
2025-09-09 13:44:50 +0200

    SH V1 V2 V4

--- genMV-V2.sh
+++ genMV-V2.sh
@@ -1,3 +1,12 @@
+#Variable
+NOM_VM="Debian1"
+VM_EXIST=$(vboxmanage list vms | grep "\"$NOM_VM\"" | wc -l)
+
+if [ "$VM_EXIST" -ne 0 ]; then
+    echo "[INFO] La machine $NOM_VM existe déjà. Suppression..."
+    VBoxManage unregistervm "$NOM_VM" --delete
+fi
+
 vboxmanage createvm --name="Debian1" --ostype="Debian_64" --register
 vboxmanage modifyvm "Debian1" --memory 4096 --cpus 2 --vram 64
 
@@ -6,6 +15,3 @@ vboxmanage storagectl "Debian1" --name "SATA Controller" --add sata --controller
 vboxmanage storageattach "Debian1" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/Debian1/Debian1.vdi"
 
 virtualbox
-echo "test"
-
-vboxmanage unregistervm "Debian1" --delete-all
