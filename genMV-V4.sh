24d483191cfea899df8a0f47688c8ec83ba623c8
merketom <tom.merkel@univ-rouen.fr>
2025-09-09 14:19:46 +0200

    V4 du SH

--- genMV-V4.sh
+++ genMV-V4.sh
@@ -1,17 +1,26 @@
-#Variable
-NOM_VM="Debian1"
-VM_EXIST=$(vboxmanage list vms | grep "\"$NOM_VM\"" | wc -l)
+arg1=$1
+arg2=$2
 
-if [ "$VM_EXIST" -ne 0 ]; then
-    echo "[INFO] La machine $NOM_VM existe déjà. Suppression..."
-    VBoxManage unregistervm "$NOM_VM" --delete
+if [ "$arg1" = "L" ]; then
+    vboxmanage list vms
+
+elif [ "$arg1" = "N" ]; then
+    vboxmanage createvm --name="$arg2" --ostype="Debian_64" --register
+    vboxmanage modifyvm "$arg2" --memory 4096 --cpus 2 --vram 64
+
+    vboxmanage createmedium disk --filename "$HOME/VirtualBox VMs/$arg2/$arg2.vdi" --size 65536 --format VDI
+    vboxmanage storagectl "$arg2" --name "SATA Controller" --add sata --controller IntelAhci
+    vboxmanage storageattach "$arg2" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/$arg2/$arg2.vdi"
+
+elif [ "$arg1" = "S" ]; then
+    vboxmanage unregistervm "$arg2" --delete-all
+
+elif [ "$arg1" = "D" ]; then
+    vboxmanage startvm $arg2
+
+elif [ "$arg1" = "A" ]; then
+    vboxmanage controlvm $arg2 poweroff
+
 fi
-
-vboxmanage createvm --name="Debian1" --ostype="Debian_64" --register
-vboxmanage modifyvm "Debian1" --memory 4096 --cpus 2 --vram 64
-
-vboxmanage createmedium disk --filename "$HOME/VirtualBox VMs/Debian1/Debian1.vdi" --size 65536 --format VDI
-vboxmanage storagectl "Debian1" --name "SATA Controller" --add sata --controller IntelAhci
-vboxmanage storageattach "Debian1" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/Debian1/Debian1.vdi"
 
 virtualbox
